bace38d054f06abc929bf3a7c97e92d63f8d7e0f
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-03-09 11:47:07 +0100

    Update global.js

--- global.js
+++ global.js
@@ -10,6 +10,11 @@ class Layout extends HTMLElement {
         
         @media screen and (max-width: 750px) {
             g-layout {
+                grid-template-rows: 200px 1fr 150px;
+            }
+        }   
+        @media screen and (max-width: 670px) {
+            g-layout {
                 grid-template-rows: 200px 1fr 250px;
             }
         }       
@@ -168,5 +173,185 @@ class Nav extends HTMLElement {
     }
 }
 
+class Footer extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `<style scoped>
+        footer, footer > div {
+            height: 100%;
+        }
+        footer {
+            position: relative;
+            display: flex;
+            justify-content: space-between;
+            width: calc(100% - 80px);
+            padding-left: 40px;
+            padding-right: 40px;
+        }
+        footer > div:nth-child(1) {
+            display: flex;
+            align-items: center;
+            gap: 40px;
+            font-weight: 700;
+        }
+        footer > div:nth-child(2) {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            text-align: right;
+        }
+        footer > div > div {
+            display: flex;
+            flex-direction: column;
+        }
+        img {
+            height: 80%;
+        }   
+        a {
+            color: #166F00;
+            font-weight: 600;
+            cursor: pointer;
+        }
+
+        @media screen and (max-width: 670px) {
+            footer {
+                flex-direction: column;
+                align-items: center;
+            }
+            footer > div {
+                height: 50%
+            }
+            footer > div:nth-child(2) {
+                text-align: center;
+                gap: 5px;
+            }
+            img {
+                height: 60%;
+            }   
+        }       
+        </style>
+        
+        <footer>
+        <div><img src="./public/logo.png"><div><span>Copyright</span><span>McDonald</span><span>© 2023</span></div></div><div><a>Conditions générales</a><a>Normes</a><a>Emplois</a><a>Provenance d'aliments</a></div>
+        </footer>`;
+    }
+}
+
+class Login extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `<style scoped> 
+        g-login {
+            min-height: 500px;
+            width: 100%;
+        }
+        g-login > div {
+            max-width: 400px;
+            width: 80%;
+            position: relative;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            display: flex;
+            flex-direction: column;
+            gap: 20px;
+            -webkit-box-shadow: 3px 7px 17px 1px rgba(61,61,61,0.49); 
+            box-shadow: 3px 7px 17px 1px rgba(61,61,61,0.49);
+            padding: 20px;
+        }
+        button {
+            text-transform: uppercase;
+            background: #FFCC04;
+            border: none;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            color: #166F00;
+            font-weight: 900;
+            font-size: 50px;
+            cursor: pointer;
+        }
+        header {
+            display: flex;
+            align-items: flex-end; 
+            gap: 10px;
+        }
+        header h2 {
+            font-size: 260%;
+        }
+        header a {
+            font-size: 200%;
+        }
+        g-login > div > div {
+            margin-top: 20px;
+            margin-bottom: 20px;
+            display: flex;
+            flex-direction: column;
+            gap: 20px;
+        }
+        g-login > div > div > div {
+            display: flex;
+            align-items: center;
+            gap: 20px;
+        }
+        g-login > div > div > div input {
+            width: calc(100% - 35px);
+        }
+        input {
+            border: none;
+            border-bottom: 2px solid #000;
+            padding: 5px;
+        }
+        @media screen and (max-width: 550px) {
+            header h2 {
+                font-size: 200%;
+            }
+            header a {
+                font-size: 160%;
+            }
+            button {
+                font-size: 40px;
+            }
+        }
+        @media screen and (max-width: 400px) {
+            g-login > div {
+                width: 85%;
+                box-shadow: none;
+            }
+            header h2 {
+                font-size: 160%;
+            }
+            header a {
+                font-size: 100%;
+            }
+            button {
+                font-size: 20px;
+            }
+        }
+        </style>
+        
+        <div>
+            <header><h2>Connexion</h2><a>S'inscrire</a></header>
+            <div>
+                <input placeholder=e-mail />
+                <div><input placeholder=password type=password /><span class="material-symbols-outlined" onclick="changeVisibility(event);">visibility</span></div>
+            </div>
+            <button>Se connecter</button>
+        </div>`;
+    }
+}
+
+
 customElements.define('g-layout', Layout);
 customElements.define('g-nav', Nav);
+customElements.define('g-footer', Footer);
+customElements.define('g-login', Login);
+
+function changeVisibility(e) {
+    const t = e.target;
+    if (t.innerText == 'visibility') {
+        t.innerHTML = 'visibility_off';
+        t.parentElement.querySelector('input').type = 'text';
+    }
+    else {
+        t.innerHTML = 'visibility';
+        t.parentElement.querySelector('input').type = 'password';
+    }
+}
